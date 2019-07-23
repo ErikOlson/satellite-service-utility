0bb6484a279399d45405fc5a4439ffe1beec6499
Erik Olson <eolson@picodigital.com>
2019-07-22 18:42:22 -0700

    Reimplemented the DishOrbital enum in a cleaner way (assigns string via constructor rather than case)

--- src/main/java/com/erikhakonolson/apps/utils/DishOrbital.java
+++ src/main/java/com/erikhakonolson/apps/utils/DishOrbital.java
@@ -2,33 +2,22 @@ package com.erikhakonolson.apps.utils;
 
 public enum DishOrbital {
 	
-	EAST_61_5,
-	EAST_72_7,
-	EAST_77,
-	WEST_110,
-	WEST_118_7,
-	WEST_119,
-	WEST_129;
+	EAST_61_5("61.5"),
+	EAST_72_7("72.7"),
+	EAST_77("77"),
+	WEST_110("110"),
+	WEST_118_7("118.7"),
+	WEST_119("119"),
+	WEST_129("129");
+	
+	private final String orbitalString;
+	
+	DishOrbital(String s) {
+		this.orbitalString = s;
+	}
 	
 	public String toString() {
-		switch (this) {
-			case EAST_61_5:
-				return "61.5";
-			case EAST_72_7:
-				return "72.7";
-			case EAST_77:
-				return "77";
-			case WEST_110:
-				return "110";
-			case WEST_118_7:
-				return "118.7";
-			case WEST_119:
-				return "119";
-			case WEST_129:
-				return "129";
-			default:
-				return null;
-				
-		}
+		return orbitalString;
 	}
+	
 }
